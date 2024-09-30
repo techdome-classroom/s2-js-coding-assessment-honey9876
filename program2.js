c8ac0ce22dba6e292a648c321b196718d01a9e14
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-09-30 14:07:54 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -14,20 +14,19 @@ var romanToInt = function(s) {
         'M': 1000
     };
     
-    // Initialize a variable to store the result
+    
     let result = 0;
     
-    // Iterate through the string
     for (let i = 0; i < s.length; i++) {
-        // Get the value of the current Roman numeral and the next one
+     
         const currentVal = romanMap[s[i]];
         const nextVal = romanMap[s[i + 1]];
 
-        // If the current value is less than the next one, subtract it
+      
         if (currentVal < nextVal) {
             result -= currentVal;
         } else {
-            // Otherwise, add the current value
+          
             result += currentVal;
         }
     }
@@ -35,5 +34,4 @@ var romanToInt = function(s) {
     return result;
 };
 
-// Export the function for use in other files or tests
 module.exports = { romanToInt };
